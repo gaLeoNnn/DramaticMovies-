8f7723415deb5ffa2c260f357a7194dd63eea2fa
Ali <Zhanabekov.ali@gmail.com>
2023-10-08 13:36:22 +0600

    small changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Carousel from "@/components/carousel/Carousel";
 import MainImage from "@/components/mainImage/MainImage";
-import Recommends from "@/components/recomends /Recommends";
+import Recommends from "@/components/recommends/Recommends";
 
 export default function Home() {
   return (
